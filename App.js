70f34681263bf6f843b9884599ad3a61752a0d69
Jaime A. Araujo <jaimeaaraujo@gmail.com>
2019-07-24 09:30:01 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import DeckList from './components/DeckList'
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <DeckList />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/DeckList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+class DeckList extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text> This is the DeckList Component</Text>
+            </View>
+        )
+    }
+}
+
+export default DeckList
